434002769e0be67fc76503bd1e7c3b9ecbdbe067
fabio.d.mota <fabio.d.mota@cgi.com>
2022-06-09 12:20:55 +0100

    new project for github and kubernetes

---
+++ src/test/java/com/catenax/valueaddedservice/ValueAddedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catenax.valueaddedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueAddedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
