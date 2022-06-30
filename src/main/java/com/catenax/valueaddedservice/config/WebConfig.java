01504f0a6727afe8e53dc09f9e8b831d14d84108
fabio.d.mota <fabio.d.mota@cgi.com>
2022-06-30 16:30:35 +0100

    try see password

--- src/main/java/com/catenax/valueaddedservice/config/WebConfig.java
+++ src/main/java/com/catenax/valueaddedservice/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.catenax.valueaddedservice.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,11 +9,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${vas.datasource.host}")
+    private String host;
+
+    @Value("${vas.datasource.password}")
+    private String password;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
+        log.info(password);
+        log.info(password);
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("*")
