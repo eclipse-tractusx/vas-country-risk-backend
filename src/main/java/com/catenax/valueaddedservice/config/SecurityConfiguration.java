585d299fac689c38a2579d74cf12e83fdd967ee8
fabio.d.mota <fabio.d.mota@cgi.com>
2022-09-07 16:37:01 +0100

    add disable security for local env

--- src/main/java/com/catenax/valueaddedservice/config/SecurityConfiguration.java
+++ src/main/java/com/catenax/valueaddedservice/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.catenax.valueaddedservice.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -34,7 +35,8 @@ public class SecurityConfiguration {
 
     @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     @Bean
-        /* package */ SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
+    @ConditionalOnProperty(prefix = "security", name = "enabled", havingValue = "true")
+        public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
         httpSecurity.httpBasic().disable();
         httpSecurity.formLogin().disable();
         httpSecurity.csrf().disable();
@@ -52,6 +54,28 @@ public class SecurityConfiguration {
                 .authenticated()
                 .and().
                 oauth2ResourceServer().jwt();
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "security", name = "enabled", havingValue = "false")
+    public SecurityFilterChain securityFilterChainLocal(final HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic().disable();
+        httpSecurity.formLogin().disable();
+        httpSecurity.csrf().disable();
+        httpSecurity.logout().disable();
+        httpSecurity.cors();
+
+        httpSecurity
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(WHITELIST)
+                .permitAll()
+                .antMatchers("/api/**")
+                .permitAll();
 
         return httpSecurity.build();
     }
